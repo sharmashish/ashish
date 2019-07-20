a273f9d401e79827823ae0c464aff5044a685e79
sharmashish <ashish.sharma17496@gmail.com>
2019-07-20 16:55:35 +0530

    third commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventsComponent } from './events/events.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MeanComponent } from './mean/mean.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectComponent } from './project/project.component';
+import { QueryComponent } from './query/query.component';
+import { ForumsComponent } from './forums/forums.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    EventsComponent,
+    SidebarComponent,
+    MeanComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    QueryComponent,
+    ForumsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
